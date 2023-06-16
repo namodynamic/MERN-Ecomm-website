4e3aca1c004a98af1b58fdfdadc32e1bca96dff6
Nnamdi Ekechi <126875351+namodynamic@users.noreply.github.com>
2023-06-16 16:02:46 +0100

    Update base_url.js

--- src/utils/base_url.js
+++ src/utils/base_url.js
@@ -1,5 +1,5 @@
 // export const base_url = "http://localhost:5000/api/";
-export const base_url = "https://mern-ecomm-backend.vercel.app/";
+export const base_url = "https://mern-ecomm-backend.vercel.app";
 
 const getTokenFromLocalStorage = localStorage.getItem("customer")
   ? JSON.parse(localStorage.getItem("customer"))
