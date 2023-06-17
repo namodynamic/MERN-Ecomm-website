8f7307d474e8af2c0349faea10bebc3c68be8664
Nnamdi Ekechi <126875351+namodynamic@users.noreply.github.com>
2023-06-17 02:21:45 +0100

    Update base_url.js

--- src/utils/base_url.js
+++ src/utils/base_url.js
@@ -1,5 +1,5 @@
 // export const base_url = "http://localhost:5000/api/";
-export const base_url = "https://mernecomm-backend.onrender.com/";
+export const base_url = "https://mernecomm-backend.onrender.com/api/";
 
 const getTokenFromLocalStorage = localStorage.getItem("customer")
   ? JSON.parse(localStorage.getItem("customer"))
